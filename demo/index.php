56708bd7af548c95ec849d937337ae7d6298cba7
Will Rossiter <will@fullscreen.io>
2016-03-04 14:41:29 +1300

    Remove warning about readme, include Helix assets into demo file

--- demo/index.php
+++ demo/index.php
@@ -6,8 +6,25 @@
         <title>Helix Demo</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--[if lt IE 9]>
+        <link href="dist/production_dumb.min.css?m=1456394536" type="text/css" rel="stylesheet">
+        <![endif]-->
+        <!--[if gte IE 9]><!-->
+        <link href="dist/production.min.css?m=1456394536" type="text/css" rel="stylesheet">
+        <!--<![endif]-->
     </head>
     <body>
+        <div class="header">
+            <h1>Helix Components</h1>
+        </div>
 
+
+
+        <div class="footer">
+            <p>Helix. Copyright by DNA Designed Limited</p>
+        </div>
+
+        <script type="text/javascript" src="dist/site.min.js"></script>
     </body>
 </html>
